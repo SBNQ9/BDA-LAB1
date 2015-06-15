249a74ef6bd5503c296521cdd7225c5f3d7dd6f1
Bommavaram <sbnq9@kc-sce463lab-08.kc.umkc.edu>
2015-06-15 15:25:50 -0500

    Initial Commit

---
+++ Tut1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tut1
+//
+//  Created by Bommavaram, Sarika  . (UMKC-Student) on 6/15/15.
+//  Copyright (c) 2015 Bommavaram, Sarika  . (UMKC-Student). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tut1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Tut1
+//
+//  Created by Bommavaram, Sarika  . (UMKC-Student) on 6/15/15.
+//  Copyright (c) 2015 Bommavaram, Sarika  . (UMKC-Student). All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
